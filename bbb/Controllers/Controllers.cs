1b71c0ec7bce14f421926762da52ee3b457a54e0
Joseph Kimathi <joseph.kimathi@bbd.co.za>
2024-04-04 00:15:23 +0200

    added get by username

--- bbb/Controllers/Controllers.cs
+++ bbb/Controllers/Controllers.cs
@@ -7,13 +7,33 @@ namespace bbb.Controllers;
 public class UserController : ControllerBase
 {
     [HttpPost("newuser")]
-    public IActionResult newUser([FromBody] UserModel model){
+    public IActionResult newUser([FromBody] UserModel model)
+    {
+        System.Console.WriteLine(Url.Link("getUser", null));
         System.Console.WriteLine($"UserModel value: {model} = {model.userID}, {model.username} ");
+        // user received well, add to db
         return Ok($"UserModel value: {model} = {model.userID}, {model.username} ");
-    } 
+    }
+
+    [HttpGet(Name = "getUser")]
+    public IActionResult getUser(string username)
+    {
+        System.Console.WriteLine("getuser: " + username);
+        int id = -1;
+        return Ok(new UserModel(username,id));
+    }
 }
 
-public class UserModel {
+public class UserModel
+{
+    public UserModel()
+    {
+    }
+    public UserModel(string name, int id)
+    {
+        username = name;
+        userID = id;
+    }
     public string username { get; set; }
     public int userID { get; set; }
 }
