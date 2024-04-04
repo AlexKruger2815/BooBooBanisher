83b8cf68fda91954de0f500561a62d262dad9083
Joseph Kimathi <joseph.kimathi@bbd.co.za>
2024-04-04 23:19:42 +0200

    more apis and strucutre

--- bbb/Controllers/Controllers.cs
+++ bbb/Controllers/Controllers.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace bbb.Controllers;
 
+private static readonly string connectionString = Configuration.GetConnectionString("DefaultConnection");
 [ApiController]
 [Route("[controller]")]
 public class UserController : ControllerBase
@@ -20,10 +23,49 @@ public class UserController : ControllerBase
     {
         System.Console.WriteLine("getuser: " + username);
         int id = -1;
-        return Ok(new UserModel(username,id));
+        return Ok(new UserModel(username, id));
     }
 }
 
+[ApiController]
+[Route("[controller]")]
+public class MessageController : ControllerBase
+{
+    [HttpGet("")]
+    public IActionResult getAllMessage(int status)
+    {
+        return Ok($"Get Msg: {status}");
+    }
+
+    [HttpGet("all")]
+    public IActionResult getAllMessage()
+    {
+        return Ok($"return all messages");
+    }
+}
+
+[ApiController]
+[Route("[controller]")]
+public class CategoryController : ControllerBase
+{
+    [HttpGet("")]
+    public IActionResult getCategories()
+    {
+        return Ok($"all categories");
+    }
+}
+
+[ApiController]
+[Route("[controller]")]
+public class SessionController : ControllerBase
+{
+    [HttpPost("")]
+    public IActionResult newSession([FromBody] SessionModel model)
+    {
+        DateTime currentDateTime = DateTime.Now;
+        return Ok($"new session model: {model.messageID} {model.userID} {model.sessionID} => {currentDateTime}");
+    }
+}
 public class UserModel
 {
     public UserModel()
@@ -36,4 +78,24 @@ public class UserModel
     }
     public string username { get; set; }
     public int userID { get; set; }
+}
+
+public class MessageModel
+{
+    public int messageID { get; }
+    public int categoryID { get; set; }
+    public string content { get; set; }
+}
+
+public class CategoryModel
+{
+    public int categoryID { get; }
+    public string categoryType { get; set; }
+}
+
+public class SessionModel
+{
+    public int sessionID { get; set;}
+    public int userID { get; set; }
+    public int messageID { get; set; } 
 }
