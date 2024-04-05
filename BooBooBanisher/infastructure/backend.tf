724cf3fa975d62b4158f8e89ba669c2424e53cda
Joseph Kimathi <Joseph.Kimathi@bbd.co.za>
2024-04-05 15:12:58 +0200

    here we og

--- BooBooBanisher/infastructure/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "BooBooBanisherbucket"
-    key = "BooBooBanisher/terraform.tfstate"  # Specify the path/key for your state file
-    region = "eu-west-1"
-  }
-}
