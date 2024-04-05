21c5fe73ba415c24bf357a11e6f1e06921cf060f
Joseph Kimathi <Joseph.Kimathi@bbd.co.za>
2024-04-05 15:24:47 +0200

    tring 1

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "BooBooBanisherbucket"
-    key = "BooBooBanisher/terraform.tfstate"  # Specify the path/key for your state file
+    bucket = "booboobanisher-infrastructure-terraform-state-bucket"
+    key = "booboobanisher-infrastructure-terraform-state-bucket/terraform.tfstate"  # Specify the path/key for your state file
     region = "eu-west-1"
   }
 }
