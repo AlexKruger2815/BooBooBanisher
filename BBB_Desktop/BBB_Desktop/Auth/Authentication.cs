237cf7dc98cb892d5f8c2bf3a453e975539eaebe
shane-theron-bbd <shane.theron@bbd.co.za>
2024-04-08 18:39:32 +0200

    MVP Desktop App

---
+++ BBB_Desktop/BBB_Desktop/Auth/Authentication.cs
@@ -0,0 +1,45 @@
+using System.Text;
+
+namespace BBB_Desktop.Auth;
+
+public class Authentication
+{
+    private readonly Dictionary<string, string?> _auth;
+
+    public bool Expired => long.Parse(_auth?.GetValueOrDefault("expires_at", null) ?? string.Empty) < DateTime.Now.ToFileTime();
+
+    public string? Bearer => $"{_auth?.GetValueOrDefault("token_type", null)} {_auth?.GetValueOrDefault("access_token", null)}";
+
+    public Authentication(string? response)
+    {
+        var pairs = response?.Split("&");
+        var parameters = new Dictionary<string, string?>();
+        foreach (var pair in pairs ?? [])
+        {
+            var keyValue = pair.Split("=");
+            parameters[keyValue[0]] = keyValue.Length > 1 ? keyValue[1].Trim() : null;
+        }
+        if (!parameters.ContainsKey("expires_at"))
+            parameters["expires_at"] = DateTime.Now.AddMilliseconds(int.Parse(parameters["expires_in"] ?? string.Empty)).ToFileTime().ToString();
+        _auth = parameters;
+    }
+
+    public override string ToString()
+    {
+        
+        var sb = new StringBuilder();
+
+        foreach (var pair in _auth)
+        {
+            sb.Append(pair.Key);
+            sb.Append('=');
+            sb.Append(pair.Value);
+            if (!pair.Equals(_auth.Last()))
+            {
+                sb.Append('&');
+            }
+        }
+
+        return sb.ToString();
+    }
+}
