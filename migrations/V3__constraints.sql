2b684a13737d320d8df7af50cdd855aa7dfb5194
Joseph Kimathi <76244122+JoeKim-17@users.noreply.github.com>
2024-04-11 11:00:56 +0200

    Update V3__constraints.sql

--- migrations/V3__constraints.sql
+++ migrations/V3__constraints.sql
@@ -1,4 +1,4 @@
-Alter table users drop constraint unique_username;
+
 ALTER TABLE users ADD CONSTRAINT unique_username UNIQUE (username);
 ALTER TABLE sessions ADD CONSTRAINT check_date CHECK (createdat <= CURRENT_TIMESTAMP);
 
