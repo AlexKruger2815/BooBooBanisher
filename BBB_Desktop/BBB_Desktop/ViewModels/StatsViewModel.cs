bd2932ba8e5437f4fe3024df8c48aed9260df805
shane-theron-bbd <shane.theron@bbd.co.za>
2024-04-12 02:19:38 +0200

    Fixed up the stats page

--- BBB_Desktop/BBB_Desktop/ViewModels/StatsViewModel.cs
+++ BBB_Desktop/BBB_Desktop/ViewModels/StatsViewModel.cs
@@ -1,12 +1,38 @@
 ﻿using BBB_Desktop.Data;
 using BBB_Desktop.Models;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace BBB_Desktop.ViewModels;
 
-public class StatsViewModel
+public class StatsViewModel : INotifyPropertyChanged
 {
     public ObservableCollection<StatsModel> Stats { get; } = new();
+
+    
+    public int Successes 
+    {
+        get { return _successes; } 
+        private set 
+        {  
+            _successes = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Successes"));
+        }
+    }
+    public int Failures
+    {
+        get { return _failures; }
+        private set
+        {
+            _failures = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Failures"));
+        }
+    }
+
+    private int _successes = 0;
+    private int _failures = 0;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
 
     public StatsViewModel()
     {
@@ -15,6 +41,7 @@ public class StatsViewModel
 
     public async Task LoadAsync(DateTime? start = null, DateTime? end = null)
     {
+        Stats.Clear();
         var stats = await BBBDBContext.GetStatsAsync(
                 (int)App.Current.Properties["userID"]!,
                 (string)App.Current.Properties["access_token"]!, 
@@ -24,5 +51,7 @@ public class StatsViewModel
         {
             Stats.Add(stat);
         }
+        Successes = Stats.Where(stat => stat.category == "Success").Count();
+        Failures = Stats.Where(stat => stat.category == "Failure").Count();
     }
 }
